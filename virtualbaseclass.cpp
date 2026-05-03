a3f3b2abfb2d9dd5db1d120cb01ef53128f528e3
Prakat Sharma <prakatsharma064@gmail.com>
2026-05-03 20:29:17 +0545

    ambiguity problem using scope resolution

---
+++ virtualbaseclass.cpp
@@ -0,0 +1,29 @@
+//A virtual base class is used to avoid duplication of base class members in multiple inheritance.
+#include <iostream>
+using namespace std;
+class A
+{
+public:
+    void show()
+    {
+        cout << "Class A\n";
+    }
+};
+class B
+{
+public:
+    void show()
+    {
+        cout << "Class B\n";
+    }
+};
+class C : public A, public B
+{
+};
+int main()
+{
+    C obj;
+    obj.A::show();
+    obj.B::show();
+    return 0;
+}
